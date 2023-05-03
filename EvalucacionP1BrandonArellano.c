e6fdce711d5aec2a66e334c5836fdfe0760d629a
Brandon Arellano <brandon.arellano@udla.edu.ec>
2023-05-03 14:56:26 -0500

    Creacion del main

---
+++ EvalucacionP1BrandonArellano.c
@@ -0,0 +1,13 @@
+/*Construya un programa para generar los n primeros términos de la serie de Fibonacci (desde el valor 0) y su respectiva suma, donde n, 
+pertenece a los enteros positivos y es ingresado por el usuario.*/
+/*Por Brandon Arellano estudiante de la carrera de Ingenieria de Software*/
+
+//Abrir biblioteca
+#include<stdio.h>
+//abrir la funcion main
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
+
